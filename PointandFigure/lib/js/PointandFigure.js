1bcf235e9f91305410d3b0b114bcf669e3a641c1
Albert Lee <Monsterblader@gmail.com>
2013-01-19 09:26:12 -0800

    Working without company names.
    Manual revert to pre Session.set implementation but retaining scrape of
    stock names.

--- PointandFigure/lib/js/PointandFigure.js
+++ PointandFigure/lib/js/PointandFigure.js
@@ -4,11 +4,11 @@ SectorList = new Meteor.Collection("sectors");
 SectorStockList = new Meteor.Collection("sectorStocksList");
 StockData = new Meteor.Collection("stockData");
 ChartHistory = new Meteor.Collection("chartHistory");
-temporary = new Meteor.Collection("temp");
 
 var DEBUGON = false;
 
 if (Meteor.isClient) {
+  // Session.set("ticker", null);
   Template.sectorMenu.sectorList = function (){
     return SectorList.find();
   };
@@ -21,29 +21,33 @@ if (Meteor.isClient) {
     return ChartHistory.find({}, {sort: {popularity: -1}});
   };
 
-  Template.headerInformation.header = function (){
-    var temp = temporary.find().fetch();
-    return temp[0] ? temp[0].name : "This is the name of your chart";
-  };
+  // Template.headerInformation.header = function (){
+  //   // Do I need to run this externally?  This seems to be running on Session.set which executes before data are collected.
+  //   // if (Session.get("ticker") !== null) {
+  //   //   console.log(Session.get("ticker"), new Date().getTime());
+  //   //   debugger
+  //   //   var temp = ChartHistory.findOne({stock: Session.get("ticker")});
+  //   //   console.log(temp, new Date().getTime());
+  //   //   return temp.name;
+  //   // } else {
+  //     return "No chart selected";
+  //   // }
+  // };
 
   Template.insertTabs.tabList = function (){
     DEBUGON && console.log("tabList");
-    var tabs = _.uniq(_.pluck(ChartHistory.find({}, {sort: {date: -1}}).fetch(), "stock")).slice(5);
-    DEBUGON && console.log("Hello???", ChartHistory.find({}, {sort: {date: -1}, limit: 10}).fetch());
-    var charts = [];
-    tabs.forEach(function(value){
-      charts.push({stock: value.stock});
-    });
     return ChartHistory.find({}, {sort: {date: -1}, limit: 10});
   };
 
   Template.renderChart.stockData = function (){
+    // None of this really does anything except trigger the .rendered function.
     DEBUGON && console.log("stockData", 1);
-    var tickerSymb = $("#tickerInput").val();
-    StockData.findOne({chart: tickerSymb})
+    var tickerSymb = Session.get("ticker");
+    StockData.findOne({chart: tickerSymb});
   };
 
   Template.renderChart.rendered = function (){
+    console.log("rendered", new Date().getTime());
     var tickerSymb = $("#tickerInput").val();
     if (StockData.findOne({chart: tickerSymb})) {
       DEBUGON && console.log("rendered");
@@ -62,6 +66,15 @@ if (Meteor.isClient) {
       });
     }
   }
+
+  // var chartSetup = function (chart){
+  //   console.log("chartSetup", new Date().getTime());
+  //   // setTimeout(function(){
+
+  //   // Session.set("ticker", chart);
+  //   // }, 2000);
+  //   Meteor.call("loadChart", chart);
+  // }
 
   Template.insertTabs.events({
     "click": function (){
@@ -100,6 +113,7 @@ if (Meteor.isServer) {
   Meteor.methods({
     /* I have only been able to find one configuration which executes/renders/rendered correctly.  That requires using the StockData collection and lines of code as noted. */
     loadChart: function (tickerSymb){
+      console.log("loadChart", new Date().getTime());
       DEBUGON && console.log("loadChart", tickerSymb);
       var todayms = new Date();
       var stockArray = [];
@@ -109,19 +123,31 @@ if (Meteor.isServer) {
       var abc = "&a=" + startDate.getMonth() + "&b=" + startDate.getDate() + "&c=" + startDate.getFullYear();
       var def = "&d=" + today.getMonth() + "&e=" + today.getDate() + "&f=" + today.getFullYear();
       var chartReq = "http://ichart.yahoo.com/table.csv?s=" + tickerSymb + abc + def + "&g=d&ignore=.csv";
+      var companyPage = "http://finance.yahoo.com/q?s=" + tickerSymb + "&ql=1";
+      Meteor.http.get(companyPage, function (err, response){
+        console.log("companyPage", new Date().getTime());
+        // Do I need to test that the entry already exists?
+        var nameIndex = response.content.search(/class=\"title\"><h2>/);
+        var result = response.content.slice(nameIndex + 18, nameIndex + 100);
+        var regex = /^[^<]+/;
+        var companyName = regex.exec(result)[0];
+        // temporary.insert({ticker: tickerSymb, name: regex.exec(result)[0]});
+        // Can this be refactored?
+        if (ChartHistory.find({stock: tickerSymb}).count() === 0) {
+          ChartHistory.insert({stock: tickerSymb, date: [new Date().getTime()], popularity: 1, name: companyName});
+          DEBUGON && console.log("ChartHistory === 0", ChartHistory.findOne({stock: tickerSymb}));
+        } else {
+          var updatedEntry = ChartHistory.findOne({stock: tickerSymb});
+          updatedEntry.date.unshift(new Date().getTime());
+          updatedEntry.popularity += 1;
+          ChartHistory.update({stock: tickerSymb}, updatedEntry);
+          DEBUGON && console.log("ChartHistory truthy", ChartHistory.findOne({stock: tickerSymb}));
+        }
+      });
       /* DO NOT REMOVE THE FOLLOWING LINE OF CODE!!!  The program does not function without the following line of code for whatever reason. */
-      if (ChartHistory.find({stock: tickerSymb}).count() === 0) {
-        ChartHistory.insert({stock: tickerSymb, date: [new Date().getTime()], popularity: 1});
-        DEBUGON && console.log("ChartHistory === 0", ChartHistory.findOne({stock: tickerSymb}));
-      } else {
-        var updatedEntry = ChartHistory.findOne({stock: tickerSymb});
-        updatedEntry.date.unshift(new Date().getTime());
-        updatedEntry.popularity += 1;
-        ChartHistory.update({stock: tickerSymb}, updatedEntry);
-        DEBUGON && console.log("ChartHistory truthy", ChartHistory.findOne({stock: tickerSymb}));
-      }
       StockData.insert({ticker: tickerSymb});
       Meteor.http.get(chartReq, function(err, response){
+        console.log("chartReq", new Date().getTime());
         stockArray = response.content
                       .split(",")
                       .slice(10)
@@ -130,13 +156,6 @@ if (Meteor.isServer) {
         /* DO NOT REMOVE THE FOLLOWING LINE OF CODE!!!  The program does not function without the following line of code for whatever reason. */
         StockData.remove({});
         StockData.insert({chart: tickerSymb, data: stockArray});        
-      });
-      var companyPage = "http://finance.yahoo.com/q?s=" + tickerSymb + "&ql=1";
-      Meteor.http.get(companyPage, function (err, response){
-        // Do I need to test that the entry already exists?
-        var nameIndex = response.content.search(/class=\"title\"><h2>/);
-        var result = response.content.slice(nameIndex + 18, nameIndex + 38);
-        temporary.insert({ticker: tickerSymb, name: result});
       });
       DEBUGON && console.log("end loadChart");
     }
