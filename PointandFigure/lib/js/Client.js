6e4a9f82e878c135947bf2067297811102249574
Albert Lee <Monsterblader@gmail.com>
2013-02-11 15:16:45 -0800

    Clean directory structure.

--- PointandFigure/lib/js/Client.js
+++
@@ -1,3 +0,0 @@
-if (Meteor.isClient) {
-  // Session.set("ticker", null);
-}
--- PointandFigure/lib/js/PointandFigure.js
+++
@@ -1,5 +0,0 @@
-SectorList = new Meteor.Collection("sectors");
-SectorStockList = new Meteor.Collection("sectorStocksList");
-ChartHistory = new Meteor.Collection("chartHistory");
-
-var DEBUGON = false;
